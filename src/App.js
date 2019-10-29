7ec2b93ce483188ac816aaacc6812511b3df699f
kkh0811 <gkim16@my.centennialcollege.ca>
2019-10-29 17:13:39 +0900

    added UI
    Added Material UI for Customer component.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,78 @@
 import React, { Component } from 'react';
 import Customer from './components/customer'
 import './App.css';
-/*
-app.js는 실질적으로 웹사이트 화면에 대한 내용 출력을 담당하는 부분
-*/
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import { Paper } from '@material-ui/core';
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth:1080
+  }
+});
 
 // customer 객체 초기화값 설정
 const customers = [
-{
-  'id':1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'GihoKim',
-  'birthday': '890811',
-  'gender': 'Male',
-  'job': 'Developer'
-},
-{
-  'id':2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': 'Obama',
-  'birthday': '510521',
-  'gender': 'Male',
-  'job': 'President'
-},
-{
-  'id':3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': 'IU',
-  'birthday': '930219',
-  'gender': 'Female',
-  'job': 'Singer'
-}
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': 'GihoKim',
+    'birthday': '890811',
+    'gender': 'Male',
+    'job': 'Developer'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': 'Obama',
+    'birthday': '510521',
+    'gender': 'Male',
+    'job': 'President'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': 'IU',
+    'birthday': '930219',
+    'gender': 'Female',
+    'job': 'Singer'
+  }
 ]
 
 // Customer 객체에 해당값 넘겨주기
 class App extends Component {
   render() {
-    return(
+    const { classes } = this.props;
+    return (
       <div>
-        {customers.map(c => {
-          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
-        })}  
+        <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            })}
+          </TableBody>
+        </Table>
+        </Paper>
       </div>
     );
   }
@@ -53,4 +85,4 @@ class App extends Component {
 이를 사용하지 않으면 자바스크립트 콘솔(Console)에 관련 오류가 출력됩니다.
 */
 
-export default App;
+export default withStyles(styles)(App);
